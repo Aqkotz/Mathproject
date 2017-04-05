4791fa44e48f61c870bb674730da838e5b209e9e
Aqkotz <andy.lyme@mac.com>
2017-04-05 16:42:05 -0400

    Update Helicopter.py

--- Helicopter.py
+++ Helicopter.py
@@ -1,5 +1,6 @@
 #Helicopter Math Project
-from math import sin, cos, tan, atan, acos, asin, radians, degrees
+#Helicopter Math Project
+from math import sin, cos, tan, atan, acos, asin, radians, degrees, sqrt
 def Cos(a):
     return cos(radians(a))
 def Sin(a):
@@ -8,18 +9,36 @@ def Tan(a):
     return tan(radians(a))
 def Atan(a):
     return degrees(atan(a))
-def Asin(a):
-    return degrees(asin(a))
-def Acos(a):
-    return degrees(acos(a))
 
 E = float(input("Please input E: "))
 A = float(input("Please input A: "))
 Z = float(input("Please input Z: "))
 Lpos = input("Please input position of L in the form x,y (no parentheses): ")
+print("")
+print("Results: ")
 Lpos = Lpos.split(",")
 Lx = float(Lpos[0])
 Ly = float(Lpos[1])
 
 TP = (1/(Tan(90-E)))*Z
 vertT = Atan((Z-61)/TP)
+Px = Cos(A)*TP
+Py = sqrt(TP*TP - Px*Px)
+LPy = Py - Ly
+LPx = Px - Lx
+LP = sqrt(LPx*LPx + LPy*LPy)
+
+AZIM = Atan(LPy/LPx)
+if A >= 90:
+    AZIM += 90
+if A >= 180:
+    AZIM += 90
+if A >= 270:
+    AZIM += 90
+print ("AZIM is " + str(AZIM))
+
+ELEV = 90 - Atan((Z-61)/LP)
+print ("ELEV is " + str(ELEV))
+
+Range = sqrt(LP*LP + (Z-61)*(Z-61))
+print ("Range is " + str(Range))
